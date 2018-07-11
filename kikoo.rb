4ae4f7cd6cea71f52aec5ee658c6a62bc5a210d0
maatcha <mattcha54@gmail.com>
2018-07-11 13:36:17 +0200

    first commit

---
+++ kikoo.rb
@@ -0,0 +1,15 @@
+
+require 'dotenv'
+# ligne très importante qui appelle la gem. Sans elle, le programme ne saura pas appeler Twitter
+require 'twitter'
+
+# quelques lignes qui enregistrent les clés d'APIs
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["FIRST_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["FIRST_CONSUMER_SECRET"]
+  config.access_token        = ENV["FIRST_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["FIRST_ACCESS_TOKEN_SECRET"]
+end
+
+# ligne qui permet de tweeter
+p client
